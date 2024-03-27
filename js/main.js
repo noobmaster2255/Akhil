8282e6a72f884916fef01cd441dc43f32638573a
Akhil Sanil <99110858+noobmaster2255@users.noreply.github.com>
2024-03-27 16:32:18 -0400

    agent listing

--- js/main.js
+++ js/main.js
@@ -1,131 +1,39 @@
-//agents
-let agentsArray = [
-  {
-    id: "agent-4613",
-    name: "Bloody Darryl The Strapped | The Professionals",
-    description:
-      "Before he was leader of the heist gang 'The Professionals' Sir Bloody Darryl was more simply called, Bloody Darryl. Still your friendly neighborhood psychopath in every sense of the word. Not actually Australian according to Australians. \\n\\n<i>I'm just gonna give them a bit of Razzle Dazzle.</i>",
-    rarity: {
-      id: "rarity_legendary_character",
-      name: "Superior",
-      color: "#d32ce6",
-    },
-    collections: [
-      {
-        id: "collection-set-op11-characters",
-        name: "Operation Riptide Agents",
-        image:
-          "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/set_icons/set_op11_characters_png.png",
-      },
-    ],
-    team: {
-      id: "terrorists",
-      name: "Terrorist",
-    },
-    image:
-      "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/characters/customplayer_tm_professional_varf5_png.png",
-  },
-  {
-    id: "agent-4619",
-    name: "'Blueberries' Buckshot | NSWC SEAL",
-    description:
-      "Known as SEAL Team's \\\"Weapons Sommelier\\\" Buckshot has thoroughly field-tested and reviewed almost every available military weapon in staggering depth. ST6 relies on Buckshot's knowledge of lesser-known guns for edge case advantages in ground based operations.\\n\\nSeen here sporting the classic NUE Type 1 uniform otherwise known as 'blueberries'. \\n\\n<i>They talk to me. The guns. They have feelings.</i>",
-    rarity: {
-      id: "rarity_mythical_character",
-      name: "Exceptional",
-      color: "#8847ff",
-    },
-    collections: [
-      {
-        id: "collection-set-op10-characters",
-        name: "Broken Fang Agents",
-        image:
-          "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/set_icons/set_op10_characters_png.png",
-      },
-    ],
-    team: {
-      id: "counter-terrorists",
-      name: "Counter-Terrorist",
-    },
-    image:
-      "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/characters/customplayer_ctm_st6_variantj_png.png",
-  },
-  {
-    id: "agent-4680",
-    name: "'Two Times' McCoy | TACP Cavalry",
-    description:
-      "'Two Times' McCoy earned his nickname by completing TACP's field training twice. Official records mark this up to a scheduling mix-up, but his teammates report restraining him from completing the trifecta.\\n\\nWith inhuman reserves of physical and mental stamina, McCoy often finds himself the last person standing at the end of any conflict—physical and verbal alike.\\n\\nHis yellow scarf is a tribute to his time with 1st Cavalry Division.\\n\\n<i>Yep. Still here.</i>",
-    rarity: {
-      id: "rarity_legendary_character",
-      name: "Superior",
-      color: "#d32ce6",
-    },
-    collections: [
-      {
-        id: "collection-set-op10-characters",
-        name: "Broken Fang Agents",
-        image:
-          "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/set_icons/set_op10_characters_png.png",
-      },
-    ],
-    team: {
-      id: "counter-terrorists",
-      name: "Counter-Terrorist",
-    },
-    image:
-      "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/characters/customplayer_ctm_st6_variantl_png.png",
-  },
-  {
-    id: "agent-4711",
-    name: "Cmdr. Mae 'Dead Cold' Jamison | SWAT",
-    description:
-      "Cmdr. 'Dead Cold' Jamison has just about seen it all. Skeptical and wry in the midst of deadly combat, others could mistake her for being nonchalant about the chaos at hand. They would be wrong. Nothing gets past Dead Cold, not bad guys with AK-47s, junior officers that call her 'Ma'am,' or an opportunity to make elite officers smirk under pressure.\\n\\n<i>You're gonna find out.</i>",
-    rarity: {
-      id: "rarity_ancient_character",
-      name: "Master",
-      color: "#eb4b4b",
-    },
-    collections: [
-      {
-        id: "collection-set-op10-characters",
-        name: "Broken Fang Agents",
-        image:
-          "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/set_icons/set_op10_characters_png.png",
-      },
-    ],
-    team: {
-      id: "counter-terrorists",
-      name: "Counter-Terrorist",
-    },
-    image:
-      "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/characters/customplayer_ctm_swat_variante_png.png",
-  },
-  {
-    id: "agent-4712",
-    name: "1st Lieutenant Farlow | SWAT",
-    description:
-      "Smart as a whip and relentlessly polite, 1st Lieutenant Farlow is one of the most beloved and respected officers in SWAT. A master tactician in hostage and barricade situations, Farlow quickly climbed the SWAT ranks. Don't be fooled by her overwhelmingly cheerful nature--she can dispatch bad guys in the blink of an eye and not think twice about it.\\n\\n<i>What are they doing over there? Drinking milkshakes?</i>",
-    rarity: {
-      id: "rarity_legendary_character",
-      name: "Superior",
-      color: "#d32ce6",
-    },
-    collections: [
-      {
-        id: "collection-set-op10-characters",
-        name: "Broken Fang Agents",
-        image:
-          "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/set_icons/set_op10_characters_png.png",
-      },
-    ],
-    team: {
-      id: "counter-terrorists",
-      name: "Counter-Terrorist",
-    },
-    image:
-      "https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/characters/customplayer_ctm_swat_variantf_png.png",
-  },
-];
+// Documentation;
+// "https://bymykel.github.io/CSGO-API/api/"
+
+const agentURL = "https://bymykel.github.io/CSGO-API/api/en/agents.json";
+let agents = [];
+
+const weaponURL = "https://bymykel.github.io/CSGO-API/api/en/skins.json";
+let weapons = [];
+
+const xhrAgents = new XMLHttpRequest();
+xhrAgents.open("GET", agentURL, false);
+xhrAgents.onreadystatechange = function () {
+  if (xhrAgents.readyState === 4) {
+    if (xhrAgents.status === 200) {
+      const data = JSON.parse(xhrAgents.responseText);
+      agents = data.slice(0, 50);
+    } else {
+      console.error("Request failed with status:", xhrAgents.status);
+    }
+  }
+};
+xhrAgents.send();
+
+const xhrWeapons = new XMLHttpRequest();
+xhrWeapons.open("GET", weaponURL, false);
+xhrWeapons.onreadystatechange = function () {
+  if (xhrWeapons.readyState === 4) {
+    if (xhrWeapons.status === 200) {
+      const data = JSON.parse(xhrWeapons.responseText);
+      weapons = data.slice(0, 50);
+    } else {
+      console.error("Request failed with status:", xhrWeapons.status);
+    }
+  }
+};
+xhrWeapons.send();
 
 //to find current page
 let currentPage = window.location.pathname;
@@ -207,7 +115,6 @@ function selectTeamButton(containerId) {
   buttonContainer.style.display = "flex";
 }
 let agentsContainer;
-let agentTile;
 if (currentPage == "/Akhil/agents.html") {
   agentsContainer = document.getElementById("agentsContainer");
   document.body.style.backgroundImage = 'url("/Akhil/assets/bg2.jpg")';
@@ -219,9 +126,8 @@ function selectedTeam(team) {
     let teams = ["terrorists", "counter-terrorists"];
     randomTeam = Math.floor(Math.random() * teams.length);
     team = teams[randomTeam];
-    console.log(team);
   }
-  let filteredAgents = agentsArray.filter(function (agent) {
+  let filteredAgents = agents.filter(function (agent) {
     return agent.team.id == team;
   });
   let agentHeading = document.getElementById("agentHeading");
@@ -229,9 +135,11 @@ function selectedTeam(team) {
   displayAgents(filteredAgents);
 }
 
+let activeTile = null;
+let activeTileId = null;
 function displayAgents(filteredAgents) {
   filteredAgents.forEach(function (agent) {
-    agentTile = document.createElement("div");
+    let agentTile = document.createElement("div");
     agentTile.setAttribute("class", "agent-tile");
     agentTile.setAttribute("id", agent.id);
     let agentImgTag = document.createElement("img");
@@ -241,14 +149,53 @@ function displayAgents(filteredAgents) {
     let agentName = document.createElement("p");
     agentName.innerHTML = agent.name;
     agentName.id = agent.id;
-
+    agentTile.classList.remove("active");
+    agentTile.addEventListener("click", () => {
+      if (activeTile !== null) {
+        activeTile.classList.remove("active");
+      }
+      agentTile.classList.add("active");
+      activeTile = agentTile;
+    });
     agentsContainer.appendChild(agentTile);
     agentTile.appendChild(agentImgTag);
     agentTile.appendChild(agentName);
   });
 }
 
-agentTile.addEventListener("click", () => {});
+let characterInfo = {};
+function saveCharacterName() {
+  let characterNameInput = document.getElementById("characterName");
+  let characterName = characterNameInput.value.trim();
+
+  if (characterName === "") {
+    alert("Name should not be empty");
+    return false;
+  }
+  if (characterName.split(" ").length > 2) {
+    alert("Name should not be more than 2 words");
+    return false;
+  }
+  if (characterName.length > 20) {
+    alert("Name should not be more than 20 characters");
+    return false;
+  }
+
+  agentId = activeTile ? activeTile.id : null;
+  if (agentId && characterName) {
+    characterInfo = {
+      agentId: agentId,
+      characterName: characterName,
+    };
+  } else {
+    alert("Select a character and type name");
+  }
+}
+
+console.log(characterInfo);
+
+
+
 
 // function for progress load
 function load() {
